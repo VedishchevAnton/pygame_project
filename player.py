da6c256a3f394cbd9f2ed2d1363b42216a11a926
Anton Vedischev <vedishchevanton@gmail.com>
2023-07-21 18:49:25 +0300

    исправлен метод move(персонаж не выходит за пределы поля)

--- player.py
+++ player.py
@@ -11,18 +11,20 @@ class Player:
 
     # метод для перемещения игрока
     def move(self):
-        """Метод для перемещения игрока"""
-        # получение списка нажатых клавиш
         keys = pygame.key.get_pressed()
-        # если нажата клавиша влево, переместить игрока влево
         if keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
-        # если нажата клавиша вправо, переместить игрока вправо
+            if self.rect.left < 0:
+                self.rect.left = 0
         if keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
-        # если нажата клавиша вверх, переместить игрока вверх
+            if self.rect.right > 800:
+                self.rect.right = 800
         if keys[pygame.K_UP]:
             self.rect.y -= self.speed
-        # если нажата клавиша вниз, переместить игрока вниз
+            if self.rect.top < 0:
+                self.rect.top = 0
         if keys[pygame.K_DOWN]:
             self.rect.y += self.speed
+            if self.rect.bottom > 600:
+                self.rect.bottom = 600
